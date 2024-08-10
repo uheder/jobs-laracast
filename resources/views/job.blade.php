762bd5c573142b482adfe467b78b612572055c87
Uheder Macedo <dev.usmacedo@outlook.com>
2024-08-10 19:32:52 -0300

    estilizado view job

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -2,7 +2,7 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2> {{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg"> {{ $job['title'] }}</h2>
     <p> This job pays {{ $job['salary'] }} per year.</p>
 </x-layout>
 
